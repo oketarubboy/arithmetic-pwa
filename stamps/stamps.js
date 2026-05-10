6c44b0b31effbb23cb23e8313632ece563e36207
oketarubboy <oketarubboy@icloud.com>
2026-05-10 22:13:16 +0900

    Add files via upload
    スタンプ10～200

--- stamps/stamps.js
+++ stamps/stamps.js
@@ -2,215 +2,286 @@
 // 画像ファイル名は「必要正解数.png」で統一しています。
 // 例: 累計150問正解で取得する画像は stamps/150.png
 // requiredCorrect は「累計正解数」です。
+// 画像ファイルは stamps フォルダに 10.png, 20.png ... 400.png の名前で追加してください。
 window.STAMP_DEFINITIONS = [
   {
-    id: "correct-1",
-    name: "はじめの一歩",
-    requiredCorrect: 1,
-    src: "stamps/1.png",
-    description: "累計1問正解で取得"
-  },
-  {
     id: "correct-10",
-    name: "10問達成",
+    name: "ネズミ",
     requiredCorrect: 10,
     src: "stamps/10.png",
     description: "累計10問正解で取得"
   },
   {
+    id: "correct-20",
+    name: "テントウムシ",
+    requiredCorrect: 20,
+    src: "stamps/20.png",
+    description: "累計20問正解で取得"
+  },
+  {
     id: "correct-30",
-    name: "30問達成",
+    name: "カエル",
     requiredCorrect: 30,
     src: "stamps/30.png",
     description: "累計30問正解で取得"
   },
   {
+    id: "correct-40",
+    name: "リス",
+    requiredCorrect: 40,
+    src: "stamps/40.png",
+    description: "累計40問正解で取得"
+  },
+  {
     id: "correct-50",
-    name: "50問チャレンジ",
+    name: "ウサギ",
     requiredCorrect: 50,
     src: "stamps/50.png",
     description: "累計50問正解で取得"
   },
   {
+    id: "correct-60",
+    name: "ハリネズミ",
+    requiredCorrect: 60,
+    src: "stamps/60.png",
+    description: "累計60問正解で取得"
+  },
+  {
+    id: "correct-70",
+    name: "ヘビ",
+    requiredCorrect: 70,
+    src: "stamps/70.png",
+    description: "累計70問正解で取得"
+  },
+  {
+    id: "correct-80",
+    name: "キツネ",
+    requiredCorrect: 80,
+    src: "stamps/80.png",
+    description: "累計80問正解で取得"
+  },
+  {
+    id: "correct-90",
+    name: "フクロウ",
+    requiredCorrect: 90,
+    src: "stamps/90.png",
+    description: "累計90問正解で取得"
+  },
+  {
     id: "correct-100",
-    name: "計算マスター",
+    name: "ワシ",
     requiredCorrect: 100,
     src: "stamps/100.png",
     description: "累計100問正解で取得"
   },
   {
+    id: "correct-110",
+    name: "オオカミ",
+    requiredCorrect: 110,
+    src: "stamps/110.png",
+    description: "累計110問正解で取得"
+  },
+  {
+    id: "correct-120",
+    name: "イノシシ",
+    requiredCorrect: 120,
+    src: "stamps/120.png",
+    description: "累計120問正解で取得"
+  },
+  {
+    id: "correct-130",
+    name: "シカ",
+    requiredCorrect: 130,
+    src: "stamps/130.png",
+    description: "累計130問正解で取得"
+  },
+  {
+    id: "correct-140",
+    name: "ワニ",
+    requiredCorrect: 140,
+    src: "stamps/140.png",
+    description: "累計140問正解で取得"
+  },
+  {
     id: "correct-150",
-    name: "150問達成",
+    name: "イルカ",
     requiredCorrect: 150,
     src: "stamps/150.png",
     description: "累計150問正解で取得"
   },
   {
+    id: "correct-160",
+    name: "マグロ",
+    requiredCorrect: 160,
+    src: "stamps/160.png",
+    description: "累計160問正解で取得"
+  },
+  {
+    id: "correct-170",
+    name: "サメ",
+    requiredCorrect: 170,
+    src: "stamps/170.png",
+    description: "累計170問正解で取得"
+  },
+  {
+    id: "correct-180",
+    name: "トラ",
+    requiredCorrect: 180,
+    src: "stamps/180.png",
+    description: "累計180問正解で取得"
+  },
+  {
+    id: "correct-190",
+    name: "クワガタ",
+    requiredCorrect: 190,
+    src: "stamps/190.png",
+    description: "累計190問正解で取得"
+  },
+  {
     id: "correct-200",
-    name: "200問達成",
+    name: "クマ",
     requiredCorrect: 200,
     src: "stamps/200.png",
     description: "累計200問正解で取得"
   },
   {
+    id: "correct-210",
+    name: "ゴリラ",
+    requiredCorrect: 210,
+    src: "stamps/210.png",
+    description: "累計210問正解で取得"
+  },
+  {
+    id: "correct-220",
+    name: "カブトムシ",
+    requiredCorrect: 220,
+    src: "stamps/220.png",
+    description: "累計220問正解で取得"
+  },
+  {
+    id: "correct-230",
+    name: "カバ",
+    requiredCorrect: 230,
+    src: "stamps/230.png",
+    description: "累計230問正解で取得"
+  },
+  {
+    id: "correct-240",
+    name: "サイ",
+    requiredCorrect: 240,
+    src: "stamps/240.png",
+    description: "累計240問正解で取得"
+  },
+  {
     id: "correct-250",
-    name: "250問達成",
+    name: "ゾウ",
     requiredCorrect: 250,
     src: "stamps/250.png",
     description: "累計250問正解で取得"
   },
   {
+    id: "correct-260",
+    name: "シャチ",
+    requiredCorrect: 260,
+    src: "stamps/260.png",
+    description: "累計260問正解で取得"
+  },
+  {
+    id: "correct-270",
+    name: "クジラ",
+    requiredCorrect: 270,
+    src: "stamps/270.png",
+    description: "累計270問正解で取得"
+  },
+  {
+    id: "correct-280",
+    name: "ライオン",
+    requiredCorrect: 280,
+    src: "stamps/280.png",
+    description: "累計280問正解で取得"
+  },
+  {
+    id: "correct-290",
+    name: "グリフォン",
+    requiredCorrect: 290,
+    src: "stamps/290.png",
+    description: "累計290問正解で取得"
+  },
+  {
     id: "correct-300",
-    name: "300問達成",
+    name: "ドラゴン",
     requiredCorrect: 300,
     src: "stamps/300.png",
     description: "累計300問正解で取得"
   },
   {
+    id: "correct-310",
+    name: "ユニコーン",
+    requiredCorrect: 310,
+    src: "stamps/310.png",
+    description: "累計310問正解で取得"
+  },
+  {
+    id: "correct-320",
+    name: "ケルベロス",
+    requiredCorrect: 320,
+    src: "stamps/320.png",
+    description: "累計320問正解で取得"
+  },
+  {
+    id: "correct-330",
+    name: "鬼",
+    requiredCorrect: 330,
+    src: "stamps/330.png",
+    description: "累計330問正解で取得"
+  },
+  {
+    id: "correct-340",
+    name: "キマイラ",
+    requiredCorrect: 340,
+    src: "stamps/340.png",
+    description: "累計340問正解で取得"
+  },
+  {
     id: "correct-350",
-    name: "350問達成",
+    name: "フェニックス",
     requiredCorrect: 350,
     src: "stamps/350.png",
     description: "累計350問正解で取得"
   },
   {
+    id: "correct-360",
+    name: "クラーケン",
+    requiredCorrect: 360,
+    src: "stamps/360.png",
+    description: "累計360問正解で取得"
+  },
+  {
+    id: "correct-370",
+    name: "麒麟",
+    requiredCorrect: 370,
+    src: "stamps/370.png",
+    description: "累計370問正解で取得"
+  },
+  {
+    id: "correct-380",
+    name: "九尾の狐",
+    requiredCorrect: 380,
+    src: "stamps/380.png",
+    description: "累計380問正解で取得"
+  },
+  {
+    id: "correct-390",
+    name: "八岐大蛇",
+    requiredCorrect: 390,
+    src: "stamps/390.png",
+    description: "累計390問正解で取得"
+  },
+  {
     id: "correct-400",
-    name: "400問達成",
+    name: "両面宿儺",
     requiredCorrect: 400,
     src: "stamps/400.png",
     description: "累計400問正解で取得"
-  },
-  {
-    id: "correct-450",
-    name: "450問達成",
-    requiredCorrect: 450,
-    src: "stamps/450.png",
-    description: "累計450問正解で取得"
-  },
-  {
-    id: "correct-500",
-    name: "500問達成",
-    requiredCorrect: 500,
-    src: "stamps/500.png",
-    description: "累計500問正解で取得"
-  },
-  {
-    id: "correct-600",
-    name: "600問達成",
-    requiredCorrect: 600,
-    src: "stamps/600.png",
-    description: "累計600問正解で取得"
-  },
-  {
-    id: "correct-700",
-    name: "700問達成",
-    requiredCorrect: 700,
-    src: "stamps/700.png",
-    description: "累計700問正解で取得"
-  },
-  {
-    id: "correct-800",
-    name: "800問達成",
-    requiredCorrect: 800,
-    src: "stamps/800.png",
-    description: "累計800問正解で取得"
-  },
-  {
-    id: "correct-900",
-    name: "900問達成",
-    requiredCorrect: 900,
-    src: "stamps/900.png",
-    description: "累計900問正解で取得"
-  },
-  {
-    id: "correct-1000",
-    name: "1000問達成",
-    requiredCorrect: 1000,
-    src: "stamps/1000.png",
-    description: "累計1000問正解で取得"
-  },
-  {
-    id: "correct-1250",
-    name: "1250問達成",
-    requiredCorrect: 1250,
-    src: "stamps/1250.png",
-    description: "累計1250問正解で取得"
-  },
-  {
-    id: "correct-1500",
-    name: "1500問達成",
-    requiredCorrect: 1500,
-    src: "stamps/1500.png",
-    description: "累計1500問正解で取得"
-  },
-  {
-    id: "correct-1750",
-    name: "1750問達成",
-    requiredCorrect: 1750,
-    src: "stamps/1750.png",
-    description: "累計1750問正解で取得"
-  },
-  {
-    id: "correct-2000",
-    name: "2000問達成",
-    requiredCorrect: 2000,
-    src: "stamps/2000.png",
-    description: "累計2000問正解で取得"
-  },
-  {
-    id: "correct-2500",
-    name: "2500問達成",
-    requiredCorrect: 2500,
-    src: "stamps/2500.png",
-    description: "累計2500問正解で取得"
-  },
-  {
-    id: "correct-3000",
-    name: "3000問達成",
-    requiredCorrect: 3000,
-    src: "stamps/3000.png",
-    description: "累計3000問正解で取得"
-  },
-  {
-    id: "correct-3500",
-    name: "3500問達成",
-    requiredCorrect: 3500,
-    src: "stamps/3500.png",
-    description: "累計3500問正解で取得"
-  },
-  {
-    id: "correct-4000",
-    name: "4000問達成",
-    requiredCorrect: 4000,
-    src: "stamps/4000.png",
-    description: "累計4000問正解で取得"
-  },
-  {
-    id: "correct-5000",
-    name: "5000問達成",
-    requiredCorrect: 5000,
-    src: "stamps/5000.png",
-    description: "累計5000問正解で取得"
-  },
-  {
-    id: "correct-6000",
-    name: "6000問達成",
-    requiredCorrect: 6000,
-    src: "stamps/6000.png",
-    description: "累計6000問正解で取得"
-  },
-  {
-    id: "correct-8000",
-    name: "8000問達成",
-    requiredCorrect: 8000,
-    src: "stamps/8000.png",
-    description: "累計8000問正解で取得"
-  },
-  {
-    id: "correct-10000",
-    name: "10000問達成",
-    requiredCorrect: 10000,
-    src: "stamps/10000.png",
-    description: "累計10000問正解で取得"
   }
 ];
